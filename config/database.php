8c38937f33e68865b7701a4d02212f95bb64239b
citrafardiani-24 <148729858+citrafardiani-24@users.noreply.github.com>
2025-06-14 11:31:05 +0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,32 @@
+<?php
+
+define('DB_HOST', 'localhost'); // Host database
+define('DB_USER', 'root');     // Username database
+define('DB_PASS', '');         // Password database
+define('DB_NAME', 'sewadaya'); // Nama database
+
+$pdo = null;
+
+/**
+ * Fungsi untuk menghubungkan ke database MySQL menggunakan PDO.
+ *
+ * @return PDO Objek PDO yang terhubung ke database.
+ * @throws PDOException Jika koneksi database gagal.
+ */
+function connectDB() {
+    global $pdo; 
+
+    if ($pdo === null) {
+        try {
+            $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec("SET time_zone = '+07:00';");
+        } catch (PDOException $e) {
+            die("<div class='p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400' role='alert'>
+                    Koneksi database gagal: " . $e->getMessage() .
+                "</div>");
+        }
+    }
+    return $pdo; 
+}
+?>
