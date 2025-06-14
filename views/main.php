2df48951bbf7d387a7ceff528c18ae26241fb87f
Mauraa16 <168169464+Mauraa16@users.noreply.github.com>
2025-06-14 11:43:53 +0700

    Add files via upload

---
+++ views/main.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+<h1 class="text-4xl font-extrabold text-gray-900 mb-8 text-center">Selamat Datang di SewaDaya</h1>
+
+<p class="text-center text-lg text-gray-700 mb-8">
+    Platform terdepan untuk menyewa pakaian adat tradisional Indonesia. Temukan pakaian yang sempurna untuk setiap acara!
+</p>
+
+
