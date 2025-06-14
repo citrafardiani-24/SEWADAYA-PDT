08bdbda69ab8779c763a8d63b941ad0ffdd211c1
citrafardiani-24 <148729858+citrafardiani-24@users.noreply.github.com>
2025-06-14 12:47:12 +0700

    database.php

--- config/database.php
+++ config/database.php
@@ -1,22 +1,17 @@
 <?php
 define('DB_HOST', 'localhost'); 
-define('DB_USER', 'root');    
+define('DB_USER', 'root');     
 define('DB_PASS', '');         
-define('DB_NAME', 'traditional_clothing_rental'); 
+define('DB_NAME', 'sewadaya'); 
 
 $pdo = null;
 
-/**
- * Fungsi untuk menghubungkan ke database MySQL menggunakan PDO.
- *
- * @return PDO Objek PDO yang terhubung ke database.
- * @throws PDOException Jika koneksi database gagal.
- */
 function connectDB() {
-    global $pdo;
+    global $pdo; 
 
     if ($pdo === null) {
         try {
+            // Buat instance PDO
             $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $pdo->exec("SET time_zone = '+07:00';");
